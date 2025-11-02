49b75744ad5cd45fd5ff180c3c914ddc530bea15
배재훈 <lange50300@gmail.com>
2025-11-02 19:53:18 +0900

    feat: 에러 및 상수 정의
    - 에러 메세지를 관리하는 ErrorMessage.java Enum 클래스를 정의합니다.
    - 정수값을 관리하는 NumberConstants.java Enum 클래스를 정의합니다.
    - 로또 랭킹을 관리하는 Rank.java Enum 클래스를 정의합니다.

---
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -0,0 +1,19 @@
+package lotto.exceptions;
+
+public enum ErrorMessage {
+    LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호 범위 내의 숫자를 입력해주세요."),
+    WINNING_NUMBER_DUPLICATE_BONUS_NUMBER("[ERROR] 로또 당첨 번호와 보너스 번호가 동일합니다."),
+    CASH_IS_NOT_DIVISIBLE("[ERROR] 구입 금액을 1,000원 단위로 입력해주세요."),
+    CASH_IS_NOT_POSITIVE("[ERROR] 구입 금액을 0원보다 크게 입력해주세요.");
+
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/utils/NumberConstants.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+public enum NumberConstants {
+
+    LOTTO_PRICE(1000),
+    ZERO(0);
+
+    private final int value;
+
+    NumberConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
