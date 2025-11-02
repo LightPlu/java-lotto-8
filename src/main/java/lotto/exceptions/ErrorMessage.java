96d124fa01fedb41088367f214b8033daba1090c
배재훈 <lange50300@gmail.com>
2025-11-02 23:06:23 +0900

    feat: 에러 메세지 추가
    - 로또 번호가 중복된다면 에러를 발생시키고 에러 메세지를 사용자에게 보여줍니다.

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호 범위 내의 숫자(1~45)를 입력해주세요."),
     WINNING_NUMBER_DUPLICATE_BONUS_NUMBER("[ERROR] 로또 당첨 번호와 보너스 번호가 동일합니다."),
     CASH_IS_NOT_DIVISIBLE("[ERROR] 구입 금액을 1,000원 단위로 입력해주세요."),
-    CASH_IS_NOT_POSITIVE("[ERROR] 구입 금액을 0원보다 크게 입력해주세요.");
+    CASH_IS_NOT_POSITIVE("[ERROR] 구입 금액을 0원보다 크게 입력해주세요."),
+    LOTTO_NUMBER_IS_NOT_DUPLICATE("[ERROR} 로또 번호는 중복될 수 없습니다.");
 
 
     private final String message;
