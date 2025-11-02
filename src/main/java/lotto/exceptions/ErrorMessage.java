1aeab9b83c509d33bcc0de8b83dcfc08825dc2c9
배재훈 <lange50300@gmail.com>
2025-11-02 22:44:12 +0900

    refactor: 에러 메세지 상세 추가
    - 로또의 범위를 숫자로 사용자에게 번호를 알려줍니다.

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.exceptions;
 
 public enum ErrorMessage {
-    LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호 범위 내의 숫자를 입력해주세요."),
+    LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호 범위 내의 숫자(1~45)를 입력해주세요."),
     WINNING_NUMBER_DUPLICATE_BONUS_NUMBER("[ERROR] 로또 당첨 번호와 보너스 번호가 동일합니다."),
     CASH_IS_NOT_DIVISIBLE("[ERROR] 구입 금액을 1,000원 단위로 입력해주세요."),
     CASH_IS_NOT_POSITIVE("[ERROR] 구입 금액을 0원보다 크게 입력해주세요.");
