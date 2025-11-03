bccfe665f66444ec4212b496df28894e3cfc23bc
배재훈 <lange50300@gmail.com>
2025-11-03 18:26:28 +0900

    feat: Service 계층 LottoService 구현
    - 각각의 도메인 서비스들을 관리하는 LottoService를 구현합니다.
    - 로또를 사고 저장하는 buyLottoAndSave메서드를 구현합니다.
    - 로또를 비교하고 집계하는 compareLottoAndAggregate메서드를 구현합니다.
    - 로또를 비교하고 결과를 반환하는 compareLotto메서드를 구현합니다.
    - 저장된 로또를 반환하는 getSavedLottos메서드를 구현합니다.

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,15 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.domain.entity.LottoResult;
+import lotto.domain.vo.Cash;
+
+public interface LottoService {
+
+    int buyLottoAndSave(int money);
+
+    LottoResult compareLotto(List<Integer> winningNumbers, int bonusNumber);
+
+    double totalWinningsMoney(Cash cash, LottoResult lottoResult);
+
+}
