21d4454071e7ff60910365e48e6202fa4370038e
배재훈 <lange50300@gmail.com>
2025-11-03 21:00:46 +0900

    feat: TypeConverter 구현
    - 입력값의 타입을 바꾸어주는 TypeConverter를 구현합니다.

---
+++ src/main/java/lotto/utils/TypeConverter.java
@@ -0,0 +1,45 @@
+package lotto.utils;
+
+import static lotto.exceptions.ErrorMessage.INPUT_VALUE_IS_INVALID;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TypeConverter {
+
+    public int priceTypeConvert(String input) {
+        int price = 0;
+        try{
+            price = Integer.parseInt(input);
+        } catch(NumberFormatException e){
+            System.out.println(INPUT_VALUE_IS_INVALID.getMessage());
+            throw new IllegalArgumentException(INPUT_VALUE_IS_INVALID.getMessage());
+        }
+        return price;
+    }
+
+    public List<Integer> winningNumberTypeConvert(String input) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        try{
+             winningNumbers = Arrays.stream(input.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch(NumberFormatException e){
+            System.out.println(INPUT_VALUE_IS_INVALID.getMessage());
+            throw new IllegalArgumentException(INPUT_VALUE_IS_INVALID.getMessage());
+        }
+        return winningNumbers;
+    }
+
+    public int bonusNumberTypeConvert(String input) {
+        int bonusNumber = 0;
+        try{
+            bonusNumber = Integer.parseInt(input);
+        } catch(NumberFormatException e){
+            System.out.println(INPUT_VALUE_IS_INVALID.getMessage());
+            throw new IllegalArgumentException(INPUT_VALUE_IS_INVALID.getMessage());
+        }
+        return bonusNumber;
+    }
+}
